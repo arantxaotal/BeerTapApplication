29d67186c9b7691d811142c7dcbc434e7960d9c4
U027606 <arantzazu.otal.ext@rsi.cajarural.es>
2023-10-30 00:34:53 +0100

    Beertap app

--- app/controllers/beer_types_controller.rb
+++ app/controllers/beer_types_controller.rb
@@ -1,5 +1,6 @@
 class BeerTypesController < ApplicationController
   def new
+    BeerType.create(name:params[:beer_types][:name],flow_volume:params[:beer_types][:flow_volume],prize_per_litre:params[:beer_types][:prize_per_litre])
     @user = User.find(params[:beer_types][:user_id])
     render 'users/tap_beer_show'
   end
