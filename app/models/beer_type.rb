f41fd6003b9a4fb69dfb8b5c316add85a9338ad6
U027606 <arantzazu.otal.ext@rsi.cajarural.es>
2023-10-30 01:04:17 +0100

    Beertap app

--- app/models/beer_type.rb
+++ app/models/beer_type.rb
@@ -1,3 +1,7 @@
 class BeerType < ApplicationRecord
   has_many :tap
+  validates :name, presence: true
+  validates :flow_volume, presence: true
+  validates :prize_per_litre, presence: true
+  validates_uniqueness_of :name
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   has_secure_password :recovery_password, validations: false
   belongs_to :tap, inverse_of: :user, optional: true
   enum role: { client: 0, dispenser: 1 }
+  validates :name, presence: true
+  validates :password_digest, presence: true
+  validates :role_cd, presence: true
+  validates_uniqueness_of :name
 end
